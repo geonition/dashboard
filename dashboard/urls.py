085ee6a43bbaecbdd78e37d6857e3ec176614ab2
Kristoffer Snabb <ksnabb@ubuntu-virtual.(none)>
2012-01-19 14:58:33 +0200

    now the dashboard works with the base_page app

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,19 +3,11 @@ from django.conf.urls.defaults import url
 
 urlpatterns = patterns('dashboard.views',
     url(r'^$',
-        'home',
-        name="dashboard"),    
-    
-    #change language
-    url(r'^setlang/$',
-        'set_language',
-        name="set_language"),
+        'dashboard',
+        name="dashboard"),
     
     url(r'^dashboard.js$',
         'dashboard_js',
         name="dashboard_js"),
 
-    url(r'^feedback/$',
-        'feedback_form',
-        name="feedback_form"),
     )
