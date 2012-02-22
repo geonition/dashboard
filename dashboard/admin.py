6056b94520fc2b5bc5cbf2ed4f42c07ea8da99f4
Kristoffer Snabb <ksnabb@ubuntu-virtual.(none)>
2012-02-22 15:24:08 +0200

    openstreetmap is now used in the amdin for adding projects

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,4 +1,4 @@
 from django.contrib.gis import admin
 from models import ProjectSetting
 
-admin.site.register(ProjectSetting, admin.GeoModelAdmin)
+admin.site.register(ProjectSetting, admin.OSMGeoAdmin)
