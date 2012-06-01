dfad82c7739cc05849f3d6b1ad59f72b23c60256
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-06-01 13:20:55 +0300

    increased the version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='dashboard',
-    version='5.0.0',
+    version='5.0.1',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/dashboard',
     packages=find_packages(),
