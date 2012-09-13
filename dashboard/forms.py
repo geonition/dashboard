2b53bfa6a314bcc36e30254e5389903b80b8f929
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-09-13 10:36:14 +0300

    feedback form is not in dashboard anymore but in the base_page

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,11 +1,3 @@
-from django.forms import ModelForm
-from models import Feedback
-
-class FeedbackForm(ModelForm):
-    """
-    This form is used to validate and send
-    feedback email from user to administrator
-    """
-    
-    class Meta:
-        model = Feedback
+"""
+put your forms here
+"""
