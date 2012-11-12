9db2d81a2d7990380e08eb4808b0b5d6efe17a5f
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-11-12 11:17:47 +0200

    the OSMadmin modified to get local openlayers.js and static files

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,4 +1,5 @@
 from django.contrib.gis import admin
+from django.core.urlresolvers import reverse_lazy
 from dashboard.models import Project
 from django.conf import settings
 from modeltranslation.admin import TranslationAdmin
@@ -20,6 +21,11 @@ class DashboardProjectAdmin(admin.OSMGeoAdmin, TranslationAdmin):
     default_zoom = getattr(settings,
                           'ORGANIZATION_ADMIN_DEFAULT_MAP_SETTINGS',
                           {'default_zoom': 4})['default_zoom']
+    
+    openlayers_url = '%s%s' % (getattr(settings, 'STATIC_URL', '/'),
+                               'js/libs/OpenLayers.js')
+    extra_js = (reverse_lazy('osmextra'),)
+   
 
 admin.site.register(Project, DashboardProjectAdmin)
 
