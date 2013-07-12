5d1a5470b95664c665ea8492a4a4f48b53443512
Sakari Ellonen <sakari.ellonen@gmail.com>
2013-07-12 11:34:07 +0300

    coupled better with no duplicate code

--- dashboard/views.py
+++ dashboard/views.py
@@ -22,54 +22,12 @@ def dashboard(request):
     PP_projects = []
     IC_projects = []
     
-    # Default url construction
-    url_prefix = 'http://'
-    if request.is_secure():
-        url_prefix = 'https://'
-    host = request.get_host()
-    # this is needed if applications are not in the root of the server
-    if request.path_info == '/':
-        path_prefix = request.path
-    else:
-        path_prefix = request.path.split(request.path_info)[0]
-    default_urls = [url_prefix + host + path_prefix + 'geoforms/active/']
     if "plan_proposals" in settings.INSTALLED_APPS:
-        default_urls.append(url_prefix + host + path_prefix + 'planning/active/')
+        PP_projects.extend(get_planning_projects()['content'])
+    if "geoforms" in settings.INSTALLED_APPS:
+        QU_projects.extend(get_questionnaires()['content'])
 
-    extra_urls = ExtraProjectUrl.on_site.order_by('-pk')
-    urls = []
-    for extra_url in extra_urls:
-        urls.append(extra_url.project_url)
-    urls.extend(default_urls)
-
-    seen = {}
-    cleaned_urls = []
-    for item in urls:
-        if item in seen: continue
-        seen[item] = 1
-        cleaned_urls.append(item)
-
-    for url in cleaned_urls:
-        if not '/active/' in url[-8:]:
-            continue
-            
-        resp = urllib2.urlopen(url)
-        if resp.getcode() == 200:
-            response_dict = json.load(resp)
-        else:
-            continue
-        if response_dict['projectType'] == 'questionnaires':
-            QU_projects.extend(response_dict['content'])
-        elif response_dict['projectType'] == 'planningProjects':
-            PP_projects.extend(response_dict['content'])
-        elif response_dict['projectType'] == 'ideaCompetitions':
-            IC_projects.extend(response_dict['content'])
-        
-#    PP_projects = Project.on_site.filter(project_type = 'PP').order_by('-pk')
-#    IC_projects = Project.on_site.filter(project_type = 'IC').order_by('-pk')
-#    QU_projects = Project.on_site.filter(project_type = 'QU').order_by('-pk')
-    
-
+    print 'pp', PP_projects
     return render_to_response('dashboard.html',
                               {'PP_projects': PP_projects,
                                'IC_projects': IC_projects,
@@ -85,4 +43,14 @@ def dashboard_js(request):
                                   context_instance = RequestContext(request))
     response['Content-type'] = 'application/javascript'
     return response
+
+def get_questionnaires():
+    from geoforms.views import get_active_questionnaires
+    return json.loads(get_active_questionnaires('').content)
+
+
+def get_planning_projects():
+    from plan_proposals.views import get_active_planning_projects
+    return json.loads(get_active_planning_projects('').content)
+
 
